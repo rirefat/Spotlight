56708fc973a8e2fb3be8dc001722e70416bb12d4
rirefat <rirefat.official@gmail.com>
2024-10-09 11:30:57 +0600

    implement data-utils function for replacing mongo ID

--- database/queries.js
+++ database/queries.js
@@ -1,6 +1,7 @@
+import { replaceMongoID } from "@/utils/data-utils";
 import { eventModel } from "./models/eventModels";
 
 export const getAllEvents = async () => {
-    const allEvents = await eventModel.find();
-    return allEvents;
+    const allEvents = await eventModel.find().lean();
+    return replaceMongoID(allEvents);
 }
---
+++ utils/data-utils.js
@@ -0,0 +1,8 @@
+export const replaceMongoID = (array) => {
+    const mappedArray = array.map(item=>{
+        return {
+            id: item._id.toString(),...item}
+    }).map(({_id, ...rest}) => rest);
+
+    return mappedArray;
+}
