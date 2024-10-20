ce0cca0fee3f9d8ed3eba4ba247bdcf41b03c50b
rirefat <rirefat.official@gmail.com>
2024-10-21 05:07:12 +0600

    done db queries for updating interested events

--- database/queries.js
+++ database/queries.js
@@ -1,7 +1,9 @@
 import { replaceMongoID } from "@/utils/data-utils";
 import { eventModel } from "./models/eventModels";
 import { userModel } from "./models/userModel";
+import mongoose from "mongoose";
 
+//====================== Queries for events ======================
 export const getAllEvents = async () => {
     const allEvents = await eventModel.find().lean();
     return replaceMongoID(allEvents);
@@ -12,6 +14,24 @@ export const getEventById = async (eventId) => {
     return event;
 }
 
+export const updateInterestedEvents = async (eventId, userId) => {
+    const event = await eventModel.findById(eventId);
+
+    if (event) {
+        const interestedUser = await eventModel.interested_ids.find((id) => id.toString() === userId);
+
+        if (interestedUser) {
+            event.interested_ids.pull(mongoose.Types.ObjectId(userId));
+        } else {
+            event.interested_ids.push(mongoose.Types.ObjectId(userId))
+        }
+
+        await event.save();
+    }
+}
+
+
+//====================== Queries for users ======================
 export const createUser = async (user) => {
     const createdUser = await userModel.create(user);
     return createdUser;
