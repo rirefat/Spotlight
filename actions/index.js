17edb0f00a62f63cb017871ac71426098253c205
rirefat <rirefat.official@gmail.com>
2024-11-05 08:41:17 +0600

    implement email sending functionality

--- actions/index.js
+++ actions/index.js
@@ -1,7 +1,9 @@
 'use server'
-import { createUser, findUser, updateGoing, updateInterestedEvents } from "@/database/queries";
+import EmailTemplate from "@/components/EmailTemplate";
+import { createUser, findUser, getEventById, updateGoing, updateInterestedEvents } from "@/database/queries";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { Resend } from "resend";
 
 
 // Action for user registration
@@ -41,9 +43,28 @@ export const addInterestedEvents = async (eventId, userId) => {
 export const addGoingEvents = async (eventId, user) => {
     try {
         await updateGoing(eventId, user?._id);
+        await sendEmail(eventId, user);
     } catch (err) {
         throw err;
     }
     revalidatePath('/');
     redirect('/');
+}
+
+
+// Actions for sending email
+export const sendEmail = async (eventId, user) => {
+    try {
+        const event = await getEventById(eventId);
+        const resend = new Resend(process.env.RESEND_API_KEY);
+        const message = `Dear ${user?.name}, you have been successfully registered for the event, ${event?.name}. Please carry this email and your official id to the venue. We are excited to have you here.`;
+        const sent = await resend.emails.send({
+            from: "noreply@noreply.tapascript.io",
+            to: user?.email,
+            subject: "Successfully Registered for the event!",
+            react: EmailTemplate({ message })
+        });
+    } catch (error) {
+        throw error;
+    }
 }
