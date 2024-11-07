da8b3a9002f7ca883ba40980fbbbb2c55286d75e
rirefat <rirefat.official@gmail.com>
2024-11-07 11:36:55 +0600

    implement meta data for individual event details page

--- app/eventDetails/[id]/page.js
+++ app/eventDetails/[id]/page.js
@@ -3,15 +3,27 @@ import EventDetails from '@/components/details/EventDetails';
 import EventVenue from '@/components/details/EventVenue';
 import { getEventById } from '@/database/queries';
 
-const DetailsPage =async ({ params: { id } }) => {
+export async function generateMetadata({ params: { id } }) {
+    const eventInfo = await getEventById(id);
+
+    return {
+        title: `Spotlight - ${eventInfo?.name}`,
+        description: eventInfo?.details,
+        openGraph: {
+            images: [eventInfo?.imageUrl]
+        }
+    }
+}
+
+const DetailsPage = async ({ params: { id } }) => {
     const event = await getEventById(id);
     return (
         <>
-            <DetailsHeroSection event={event}/>
+            <DetailsHeroSection event={event} />
             <section className="container">
                 <div className="grid grid-cols-5 gap-12 my-12">
-                    <EventDetails details={event?.details}/>
-                    <EventVenue location={event?.location}/>
+                    <EventDetails details={event?.details} />
+                    <EventVenue location={event?.location} />
                 </div>
             </section>
         </>
