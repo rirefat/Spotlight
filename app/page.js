b5e136199037f711d25a3497a4916f2dfa225832
rirefat <rirefat.official@gmail.com>
2024-11-09 06:54:35 +0600

    modify suspense fallback component

--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
 import EventList from "@/components/landingPage/EventList";
 import Header from "@/components/landingPage/Header";
+import Loading from "@/components/Loading";
 import { Suspense } from "react";
 
 const Home = ({ searchParams: { query } }) => {
   return (
     <section className="container">
       <Header />
-      <Suspense key={query} fallback={<p>Loading event data...</p>}>
+      <Suspense key={query} fallback={<Loading />}>
         <EventList query={query} />
       </Suspense>
     </section>
---
+++ components/Loading.jsx
@@ -0,0 +1,7 @@
+const Loading = () => {
+    return (
+        <p className="text-center my-10 text-gray-400">Loading event data...</p>
+    );
+};
+
+export default Loading;
