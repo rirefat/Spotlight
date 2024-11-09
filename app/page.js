de00ec7063f3f90fc27a8fed13c3352faedc24fc
rirefat <rirefat.official@gmail.com>
2024-11-09 06:50:04 +0600

    use suspense for data loading

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import EventList from "@/components/landingPage/EventList";
 import Header from "@/components/landingPage/Header";
+import { Suspense } from "react";
 
 const Home = ({ searchParams: { query } }) => {
   return (
     <section className="container">
       <Header />
-      <EventList query={query} />
+      <Suspense key={query} fallback={<p>Loading event data...</p>}>
+        <EventList query={query} />
+      </Suspense>
     </section>
   );
 };
